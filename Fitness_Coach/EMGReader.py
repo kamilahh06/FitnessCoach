2ad464930a71f3d063c2c84cb56d5f5aad7c5459
felizy-a <alicia.richard@icloud.com>
2025-09-22 12:41:13 -0500

    Update EMGReader.py

--- Fitness_Coach/EMGReader.py
+++ Fitness_Coach/EMGReader.py
@@ -3,13 +3,15 @@ import time
 import serial
 
 class EMGReader:
-   def __init__(self, port, baud=115200)
+    def __init__(self, port, baud=115200):
         self.ser = serial.Serial(port, baud, timeout=1)
     
-    #read one line and return (time_ms, emg_value) as ints
+    # read one line and return (time_ms, emg_value) as ints
     def read_line(self):
-        line = ser.readline().decode("utf-8", errors="ignore").strip()
-            if line:
+        line = self.ser.readline().decode("utf-8", errors="ignore").strip()
+        if line:
+            parts = line.split()
+            if len(parts) >= 2:
                 try:
                     t = int(parts[0])
                     val = int(parts[1])
@@ -18,10 +20,10 @@ class EMGReader:
                     return None
         return None
 
-    #stream everything to a csv file until ctrl+c
+    # stream everything to a csv file until ctrl+c
     def log_to_file(self, filename):
         with open(filename, "w") as f:
-            f.write("time_ms, emg_value\n") #headeer
+            f.write("time_ms, emg_value\n") # header
             try:
                 while True:
                     row = self.read_line()
