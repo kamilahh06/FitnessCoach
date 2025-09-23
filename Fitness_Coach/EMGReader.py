28af804fd8e4095621cc58906137dfcfe0d34d13
Kamilah Clark <kamilah@cs.utexas.edu>
2025-09-23 16:40:44 -0500

    Updated preprocessing methods and EMGReader

--- Fitness_Coach/EMGReader.py
+++ Fitness_Coach/EMGReader.py
@@ -1,6 +1,7 @@
 import random
 import time
 import serial
+import numpy as np
 
 class EMGReader:
     def __init__(self, port, baud=115200):
@@ -19,9 +20,36 @@ class EMGReader:
                 except ValueError:
                     return None
         return None
+    
+    #read a window of emg values of given size
+    def read_window(self, window_size=30):
+        window = np.array([])
+        start_time = time.time()
+        while len(window) < window_size:
+            row = self.read_line()
+            if row:
+                window.append(row[1])
+        return window, start_time
+    
+    #create a new log file with header
+    def create_file(self):
+        filename = f"emg_log_{int(time.time())}.csv"
+        print(f"Logging to {filename}...")
+        with (open filename, "w") as f:
+            f.write("time_ms, emg_value\n")
+        return filename
+    
+    #create a new log file with header
+    def log_to_file(self, filename):
+        if not filename:
+            filename = self.create_file()
+        with open(filename, "w") as f:
+            row = self.read_line()
+            f.write(f"{row[0]},{row[1]}\n")
+         
 
     #stream everything to a csv file until ctrl+c
-    def log_to_file(self, filename):
+    def log_all_to_file(self, filename):
         with open(filename, "w") as f:
             f.write("time_ms, emg_value\n") #header
             try:
--- Fitness_Coach/preprocessing.py
+++ Fitness_Coach/preprocessing.py
@@ -1,5 +1,36 @@
 import numpy as np
 from scipy.signal import butter, lfilter
 
+
 class Processor:
-    """ Placeholder for EMG signal processing logic. """
+   def __init__(self, fs=30.0, lowcut=0.1, highcut=3.0, order=5):
+       # self.fs = fs
+       # self.lowcut = lowcut
+       # self.highcut = highcut
+       # self.order = order
+  
+   # FILTERING FUNCTIONS
+   def bandpass_filtering(self, data):
+       """
+       Apply a bandpass filter to the data.
+       Outputs frequences between lowcut and highcut.
+       """
+       nyq = 0.5 * self.fs
+       low = self.lowcut / nyq
+       high = self.highcut / nyq
+       b, a = butter(self.order, [low, high], btype='band')
+       y = lfilter(b, a, data)
+       return y
+  
+  
+   # OTHER PREPROCESSING FUNCTIONS
+   def detrending(self, data):
+       return data - np.mean(data)
+  
+   def normalize(self, data):
+       return (data - np.min(data)) / (np.max(data) - np.min(data))
+  
+  
+  
+  
+
